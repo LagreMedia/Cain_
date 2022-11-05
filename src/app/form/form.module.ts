87e5d340145c1c0ade3b20fde7a2ef2f430dde90
Cwagne17 <cwagne17@students.towson.edu>
2022-11-05 09:16:36 -0400

    adds footer component

--- src/app/form/form.module.ts
+++ src/app/form/form.module.ts
@@ -2,11 +2,13 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule, NO_ERRORS_SCHEMA } from '@angular/cor
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormComponent } from './form.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
+    SharedModule,
     RouterModule.forChild([
       { path: '', component: FormComponent }
     ])
