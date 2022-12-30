a0eef4f9972a023a51ca1ad2980fe92735344d6b
Josue Morales <molinajosue92@hotmail.com>
2022-12-30 16:12:48 -0500

    updates to how copy is stored

--- src/app/shared/content/content.component.ts
+++ src/app/shared/content/content.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { COPY } from '../copy/copy';
 
 @Component({
   selector: 'app-content',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content.component.scss']
 })
 export class ContentComponent implements OnInit {
+  copy = COPY;
 
   constructor() { }
 
